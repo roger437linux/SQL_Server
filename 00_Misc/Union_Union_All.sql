a543c9b548baf07b6b2d70c699de182a485a02f5
ROGERIO DA SILVA ALCANTARA <sn1024493@educ123.sp.senai.br>
2026-09-17 12:04:21 -0300

    Add Misc folder

---
+++ 00_Misc/Union_Union_All.sql
@@ -0,0 +1,17 @@
+
+-- Union -> Combinado resultados de queries e remove duplicatas
+
+SELECT 7 UNION SELECT 4;
+
+SELECT 3 UNION SELECT 3;
+
+-- Union all -> Combina resultados de queries mantendo duplicatas
+
+SELECT 7 UNION ALL SELECT 4;
+
+SELECT 5 UNION ALL SELECT 5;
+
+
+--  Uma regra importante:
+--  O resultado das consultas precisam ter a
+--  mesma qtde de colunas e tipos compatíveis.
--- 01_metalurgica_vitoria/aula-3.sql
+++ 01_metalurgica_vitoria/aula-3.sql
@@ -1,58 +1,61 @@
-insert into dbo.Fornecedores (nome_fornecedor, cnpj, telefone, cidade)
-values
+INSERT INTO dbo.Fornecedores (nome_fornecedor, cnpj, telefone, cidade)
+VALUES
 ('Iron Maiden', '02.053.726/0001-18', '1234-5678', 'Belém'),
 ('Led Zeppelin', '89.181.585/0001-05', '7777-7777', 'Porto Alegre'),
 ('Pink Floyd', '70.407.737/0001-09', '99999-99999-', 'São Paulo'),
 ('Deep Purple', '01.261.363/0001-43', '3333-3333', 'Recife'),
 ('Beatles', '86.030.518/0001-93', '7878-7878', 'Salvador');
 
-insert into dbo.Produtos (nome_produto, categoria_produto, qtde_estoque_produto, preco_uni_produto, id_fornecedor)
-values
+INSERT INTO dbo.Produtos (nome_produto, categoria_produto, qtde_estoque_produto, preco_uni_produto, id_fornecedor)
+VALUES
 ('Leite', 'Alimentos', 100, 5.5, 2006),
 ('Sabão em pó', 'Limpeza', 150, 3.99, 2005),
 ('Detergente', 'Limpeza', 200, 12.5, 2005),
 ('Sal', 'Alimentos', 20, 8.5, 2006),
 ('Açucar', 'Alimentos', 200, 5.5, 2006);
 
-select * from dbo.Fornecedores;
+SELECT * FROM dbo.Fornecedores;
 
-select * from dbo.Produtos;
+SELECT * FROM dbo.Produtos;
 
 
 -- Funções de agregação
 -- Filtros e funções de agregação: where, count, sum, avg, max, min, etc.
 
+-- Qtde de produtos cadastrados
 select count(*) as qtde_produtos from dbo.Produtos;
 
-select sum(qtde_estoque_produto) as qtde_total_estoque from dbo.Produtos;
+-- Qtde total de produtos no estoque
+SELECT SUM(qtde_estoque_produto) AS qtde_total_estoque FROM dbo.Produtos;
 
-select min(nome_produto) from dbo.Produtos;
+-- Produto cadastrado com nome que inicia com menor letra
+SELECT MIN(nome_produto) FROM dbo.Produtos;
 
 
 -- Produto mais barato e mais caro
-select min(preco_uni_produto) as "Menor preço R$", max(preco_uni_produto) as "Maior preço R$"
-from dbo.Produtos;
+SELECT MIN(preco_uni_produto) AS "Menor preço R$", MAX(preco_uni_produto) As "Maior preço R$"
+FROM dbo.Produtos;
+
+-- Produto mais barato e mais caro indicando o nome
+-- Usando Union e subqueries
+SELECT nome_produto, preco_uni_produto AS "Menor preço R$"
+FROM dbo.Produtos
+WHERE preco_uni_produto = (SELECT MIN(preco_uni_produto) FROM dbo.Produtos)
+
+UNION ALL
+
+SELECT nome_produto, preco_uni_produto AS "Maior preço R$"
+FROM dbo.Produtos
+WHERE preco_uni_produto = (SELECT MAX(preco_uni_produto) FROM dbo.Produtos);
+
 
 -- Soma do estoque total por categoria
-select categoria_produto, sum(preco_uni_produto) as "Preço total p/ categoria"
-from dbo.Produtos
-group by categoria_produto
-order by "Preço total p/ categoria" asc;
+SELECT categoria_produto, SUM(preco_uni_produto) AS "Preço total p/ categoria"
+FROM dbo.Produtos
+GROUP BY categoria_produto
+ORDER BY "Preço total p/ categoria" ASC;
 
 -- Qtde de fornecedores cadastrados apenas em São Paulo / em uma região específica
-select count(*) as qtde_fornecedores_sp from Fornecedores
-where cidade =  'São Paulo';
-
-select * from dbo.Fornecedores;
-
-
-
-
-
-
-
-
-
-
-
+SELECT COUNT(*) AS qtde_fornecedores_sp FROM Fornecedores
+WHERE cidade =  'São Paulo';
 
